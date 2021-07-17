c51381e725a3f9b875a34ee41ff9ef021fc6622e
RizwanMushtaq <rizwanmushtaq15@gmail.com>
2021-07-17 16:11:13 +0200

    update database config file to heroku db

--- php/config.php
+++ php/config.php
@@ -1,8 +1,8 @@
 <?php
 
-    $host = "localhost";
-    $dbbenutzer = "root";
-    $dbpasswort = "";
-    $dbname = "doitnow";
+    $host = "eu-cdbr-west-01.cleardb.com";
+    $dbbenutzer = "ba7aaee6a9bc02";
+    $dbpasswort = "b152712d";
+    $dbname = "heroku_d99c6ab52e8261c";
 
 ?>
